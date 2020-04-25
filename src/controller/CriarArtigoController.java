b4acae06dfd0a22a966d4ca088c0623d65d16794
Alexia Gabrielly <alexiagabrielly@ibm.com>
2020-04-25 20:52:33 -0300

    Fazer model de artigos

---
+++ src/controller/CriarArtigoController.java
@@ -0,0 +1,8 @@
+package controller;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet("/PublicarArtigo.do")
+public class CriarArtigoController extends HttpServlet {
+}
---
+++ src/model/Artigos.java
@@ -0,0 +1,67 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Artigos<Comentario> {
+    private int id;
+    private String titulo;
+    private String descricao;
+    private String texto;
+    private ArrayList<Comentario> comentario;
+
+    public Artigos(int id, String titulo, String descricao, String texto, ArrayList<Comentario> comentario) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.texto = texto;
+        this.comentario = comentario;
+    }
+
+    public Artigos() {
+        this(0, "", "", "", null);
+    }
+
+    //Métodos de get
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public String getDescricao() {
+        return this.descricao;
+    }
+
+    public String getTexto() {
+        return this.texto;
+    }
+
+    public ArrayList<Comentario> getComentario() {
+        return this.comentario;
+    }
+
+    //Métodos de set
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public void setTexto(String texto) {
+        this.texto = texto;
+    }
+
+    public void setComentario(ArrayList<Comentario> comentario) {
+        this.comentario = comentario;
+    }
+
+}
