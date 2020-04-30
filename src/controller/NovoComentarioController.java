27da5df9a1ea219b7556473a545dff9f1e244a71
vitsaojudas <vitoriasalata.3861@aluno.saojudas.br>
2020-04-30 20:33:02 -0300

    Criar controller de comentário

---
+++ src/controller/NovoComentarioController.java
@@ -0,0 +1,43 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import model.*;
+import service.*;
+
+@WebServlet("/NovoComentario.do")
+public class NovoComentarioController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doPost(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        int idArtigos = (Integer) session.getAttribute("artigo-id");
+        
+        String nome = request.getParameter("nome");
+        String texto = request.getParameter("texto");
+
+        Artigos art = new Artigos();
+        art.setId(idArtigos);
+        Comentario com = new Comentario(0, nome, texto, news);
+
+        ArtigoService atrService = new ArtigoService();
+        atrService.create(com);
+
+        PrintWriter out = response.getWriter();
+        out.println("Comentario criado com sucesso");
+    }
+}
