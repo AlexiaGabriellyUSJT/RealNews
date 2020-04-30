a8728e446510f32f3f1ed3bb7a99d2b2c9eea16c
Alexia Gabrielly <alexiagabrielly@ibm.com>
2020-04-30 16:29:26 -0300

    Arrumar método de listar um único artigo

--- src/dao/ArtigoDAO.java
+++ src/dao/ArtigoDAO.java
@@ -97,6 +97,61 @@ public class ArtigoDAO {
             e.printStackTrace();
         }
 	}
+
+	//Método Read para um único artigo
+	public Artigos exibirArtigo(Artigos atr) {
+		String exibirArtigo = "SELECT * FROM noticia WHERE id = ?;";
+		
+		try(PreparedStatement pst = connection.prepareStatement(exibirArtigo)) {
+			
+			pst.setInt(1, atr.getId());
+			ResultSet resultado = pst.executeQuery();
+			Artigos artigo = new Artigos();
+			
+			if(resultado.next()) {
+				int id = resultado.getInt("id");
+                String titulo = resultado.getString("titulo");
+                String descricao = resultado.getString("descricao");
+                String texto = resultado.getString("texto");
+
+                artigo.setId(id);
+                artigo.setTitulo(titulo);
+                artigo.setDescricao(descricao);
+                artigo.setTexto(texto);
+			}
+			
+			return artigo;
+			
+		} catch (SQLException e) {
+            System.err.println("Falha no banco: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            System.err.println("Falha no java: " + e.getMessage());
+            e.printStackTrace();
+        }
+		
+		return null;
+	}
+	
+	//Método Update
+	
+public void updateArtigo(Artigos atr) {
+	String updateArtigo = "UPDATE noticia SET titulo = ?, descricao = ?, texto = ? WHERE id = ?;";
+	
+	try(PreparedStatement pst = connection.prepareStatement(updateArtigo)) {
+		pst.setString(1, atr.getTitulo());
+		pst.setString(2, atr.getDescricao());
+		pst.setString(3, atr.getTexto());
+		pst.setInt(4, atr.getId());
+		pst.execute();
+	} catch (SQLException e) {
+        System.err.println("Falha no banco: " + e.getMessage());
+        e.printStackTrace();
+    } catch (Exception e) {
+        System.err.println("Falha no java: " + e.getMessage());
+        e.printStackTrace();
+    }
+}
 	
 
 }
