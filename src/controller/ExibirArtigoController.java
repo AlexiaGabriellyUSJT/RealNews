b2186bb3c3ddcc2b41784d4ad6f23464f1fd1a00
vitsaojudas <vitoriasalata.3861@aluno.saojudas.br>
2020-04-30 21:32:59 -0300

    Arrumar estilo de criação de comentarios

--- src/controller/ExibirArtigoController.java
+++ src/controller/ExibirArtigoController.java
@@ -38,21 +38,27 @@ public class ExibirArtigoController extends HttpServlet {
     	ArrayList<Comentario> comentarios = comService.read(atr);
     	
     	PrintWriter out = response.getWriter();
-    	out.println("<!DOCTYPE html><html lang=\"pt-br\"><head><meta charset='UTF-8'><link rel='stylesheet' type='text/css' href='style/main-style.css' media='screen' /><title>RealNews</title></head>");
+    	out.println("<!DOCTYPE html><html lang=\"pt-br\"><head><meta charset='UTF-8'><link rel=\"stylesheet\" href=\"./style/cadastro-artigo.css\"><link rel='stylesheet' type='text/css' href='style/main-style.css' media='screen' /><title>RealNews</title></head>");
     	out.println("<header><span class=\"page-title\">Real News</span><nav><a class=\"publicar-btn\" href=\"./CadastrarArtigo.html\">Novo artigo</a></nav></header>");
     	out.println("<div class=\"atr-wrapper\">");
     	out.println("<div>");
     	out.println("<h3>" + artigo.getTitulo() + "</h3>");
     	out.println("<p>" + artigo.getTexto() + "</p>");
-        out.println("<div class='comentarios-content'><p class='titulo'> Comentarios</p>");
+        out.println("<div class='comentarios-content'><h3 class='titulo'> Comentarios</h3>");
         
+        out.println("<div class='comentarios-wrapper'>");
         for(Comentario c : comentarios) {
             out.println("<div class='comentario'><p class='nome'>"+c.getNome()+"</p><p class='texto'>"+c.getTexto()+"</p></div>");
         }
         
         out.println("</div>");
-        out.println("<form action='NovoComentario.do' method='post' class='novo-comentario '><p class='titulo '>Adicionar novo comentario</p> <input type='text' name='nome' placeholder='Nome' /><textarea type='text' name='texto' placeholder='Escreva aqui seu comentario'></textarea><button type='submit'>Adicionar</button></form>");
-        out.println("<form action='NoticiasUser.do' method='get' class='novo-comentario '><button type='submit'>Voltar</button></form>");
+        out.println("<form action='NovoComentario.do' method='post' class='novo-comentario'>");
+        out.println("<p class='titulo '>Adicionar novo comentario</p>");
+        out.println("<input type='text' name='nome' placeholder='Nome'/>");
+        out.println("<textarea type='text' name='texto' placeholder='Escreva aqui seu comentario'></textarea>");
+        out.println("<button class=\"publicar-btn\" type='submit'>Adicionar</button>");
+        out.println("</form>");
+        out.println("</div>");
     	out.println("</div>");
     	out.println("</div>");
     	
